cfc245f3c89ad8f62893f0f759355ae2837bef26
waitingqin <1104851410@qq.com>
2022-12-19 19:14:17 +0800

    过一号点

---
+++ cpu1/MEM.v
@@ -0,0 +1,75 @@
+`include "lib/defines.vh"
+module MEM(
+    input wire clk,
+    input wire rst,
+    // input wire flush,
+    input wire [`StallBus-1:0] stall,
+
+    input wire [`EX_TO_MEM_WD-1:0] ex_to_mem_bus,
+    input wire [31:0] data_sram_rdata,
+
+    output wire [`MEM_TO_WB_WD-1:0] mem_to_wb_bus,
+    output wire mem_wreg,
+    output wire [4:0] mem_waddr,
+    output wire [31:0] mem_wdata,
+    output wire mem_opl
+);
+
+    reg [`EX_TO_MEM_WD-1:0] ex_to_mem_bus_r;
+
+    always @ (posedge clk) begin
+        if (rst) begin
+            ex_to_mem_bus_r <= `EX_TO_MEM_WD'b0;
+        end
+        // else if (flush) begin
+        //     ex_to_mem_bus_r <= `EX_TO_MEM_WD'b0;
+        // end
+        else if (stall[3]==`Stop && stall[4]==`NoStop) begin
+            ex_to_mem_bus_r <= `EX_TO_MEM_WD'b0;
+        end
+        else if (stall[3]==`NoStop) begin
+            ex_to_mem_bus_r <= ex_to_mem_bus;
+        end
+    end
+
+    wire [31:0] mem_pc;
+    wire data_ram_en;
+    wire [3:0] data_ram_wen;
+    wire sel_rf_res;
+    wire rf_we;
+    wire [4:0] rf_waddr;
+    wire [31:0] rf_wdata;
+    wire [31:0] ex_result;
+    wire [31:0] mem_result;
+
+    assign {
+        mem_pc,         // 75:44
+        data_ram_en,    // 43
+        data_ram_wen,   // 42:39
+        sel_rf_res,     // 38
+        rf_we,          // 37
+        rf_waddr,       // 36:32
+        ex_result       // 31:0
+    } =  ex_to_mem_bus_r;
+    
+
+
+    assign mem_result = data_sram_rdata;
+    assign rf_wdata = sel_rf_res ? mem_result : ex_result;
+
+    assign mem_to_wb_bus = {
+        mem_pc,     // 69:38
+        rf_we,      // 37
+        rf_waddr,   // 36:32
+        rf_wdata    // 31:0
+    };
+    
+    
+    assign mem_wreg = rf_we;
+    assign mem_waddr = rf_waddr;
+    assign mem_wdata = rf_wdata;
+
+
+
+
+endmodule
